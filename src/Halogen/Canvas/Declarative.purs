e70806e83108ab15cc189f12592259b8e65c1f20
UnrelatedString <unrelatedstring@gmail.com>
2025-03-28 12:19:06 -0400

    uh ohhh

--- src/Halogen/Canvas/Declarative.purs
+++ src/Halogen/Canvas/Declarative.purs
@@ -1,8 +1,20 @@
 module Halogen.Canvas.Declarative
- ( uhh
+ ( canvas
+ , CanvasInput
  ) where
 
 import Prelude
 
-uhh :: Unit
-uhh = unit
+import Effect (Effect)
+import Effect.Class (class MonadEffect, liftEffect)
+import Halogen as H
+import Halogen.HTML as HTML
+import Halogen.HTML.Events as Event
+
+type CanvasInput
+
+-- leaving queries and events as a TODO for now
+canvas ::
+    forall query output m.
+    MonadEffect m =>
+    H.Component query  output m
