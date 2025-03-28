b8ea6aa78e50ac9b1f8b52c134f870267db2290a
UnrelatedString <unrelatedstring@gmail.com>
2025-03-28 12:50:26 -0400

    wait .... how was this supposed to actually *work* again ???

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 ![Latest Version Tag](https://img.shields.io/github/v/tag/UnrelatedString/purescript-halogen-declarative-canvas)
 [![Pursuit](https://pursuit.purescript.org/packages/purescript-halogen-declarative-canvas/badge?)](https://pursuit.purescript.org/packages/purescript-halogen-declarative-canvas)
 
-A declaratively input-driven canvas component for Halogen.
-
-Though developed alongside `massara-na-canvas`, this can also be used with the raw `canvas` API or any other wrapper around it.
+A declaratively input-driven canvas component for Halogen, accepting raw `canvas` effects.
--- src/Halogen/Canvas/Declarative.purs
+++ src/Halogen/Canvas/Declarative.purs
@@ -10,11 +10,52 @@ import Effect.Class (class MonadEffect, liftEffect)
 import Halogen as H
 import Halogen.HTML as HTML
 import Halogen.HTML.Events as Event
+import Graphics.Canvas
+ ( CanvasElement
+ , Context2D
+ , getContext2D
+ , setCanvasDimensions
+ )
 
-type CanvasInput
+type CanvasInput =
+  { width :: Int
+  , height :: Int
+  , draw :: Context2D -> Effect Unit
+  }
+
+type State = CanvasInput
 
 -- leaving queries and events as a TODO for now
+type Action = Unit
+
 canvas ::
-    forall query output m.
-    MonadEffect m =>
-    H.Component query  output m
+  forall query output m.
+  MonadEffect m =>
+  H.Component query CanvasInput output m
+canvas = H.mkComponent
+  { initialState
+  , render
+  , eval: H.mkEval evalSpec
+  }
+
+initialState :: CanvasInput -> State
+
+render ::
+  forall m.
+  MonadEffect m =>
+  State -> H.ComponentHTML Action () m
+render rec = HTML.canvas
+  [ Prop.width rec.width
+  , Prop.height rec.height
+  ]
+
+evalSpec :: H.EvalSpec
+evalSpec = H.defaultEval
+--  { handleAction = handleAction
+--  }
+
+-- handleAction ::
+--   forall output m.
+--   MonadEffect m =>
+--   Action ->
+--   H.HalogenM State Action () output m Unit
